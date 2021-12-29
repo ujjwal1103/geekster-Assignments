0c78387e891d754e61d9637d9972432d14d86162
ujjwal1103 <84891822+ujjwal1103@users.noreply.github.com>
2021-12-29 11:37:36 +0530

    Create script.js

---
+++ dec21/Assignment2/script.js
@@ -0,0 +1,11 @@
+	console.log("dec_21 Assignment No 2")
+    //Check whether the given chracter is a vowel or not. (Also check for capital characters).
+    
+	var char = 'A'
+
+	if(char=='A'|| char =='a'||char == 'e'||char == 'E'||char == 'i'||char == 'I'||char == 'o'||char == 'O'||char == 'u'||char == 'U'){
+		alert("It is a vowel");
+	}
+	else{
+		alert("it is a consonant");
+	}
