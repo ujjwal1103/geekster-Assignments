8bbd84778483858a10bfbc83c4cffdd04d93fd67
ujjwal1103 <84891822+ujjwal1103@users.noreply.github.com>
2021-12-29 11:28:23 +0530

    Add files via upload

---
+++ dec21/Assignment1/script.js
@@ -0,0 +1,20 @@
+console.log("dec_21 Assignment No 1")
+//Take 3 inputs from the user and show minimum of the three. 
+var a ,b,c;
+		a = 90;
+		b = 10;
+		c = 60;
+		if(a<b){
+			if(a<c){
+				
+			console.log("The a is minimum number");
+			}
+		}
+		else if(b<c){
+			if(b<a){
+			console.log("The b is minimum number");
+			}
+		}
+		else{
+			console.log("The c is minimum number");
+		}
