e53eb3975c861a623e1ccdc9c8fd036ec73affae
ujjwal1103 <84891822+ujjwal1103@users.noreply.github.com>
2021-12-29 11:10:24 +0530

    Add files via upload

---
+++ dec20/Assignment2/script.js
@@ -0,0 +1,20 @@
+	console.log("dec_20 Assignment No 2")
+    //"Write a program to have 3 variables (all three are number) and output the highest/greatest///maximum of all three."
+    var a ,b,c;
+		a = 90;
+		b = 10;
+		c = 60;
+		if(a>b){
+			if(a>c){
+				
+			console.log("The a is greater");
+			}
+		}
+		else if(b>c){
+			if(b>a){
+			console.log("The b is greater");
+			}
+		}
+		else{
+			console.log("The c is greater");
+		}
