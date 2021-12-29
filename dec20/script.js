9dcba196f6197619c9dd93546d9a1e43d5d8ef48
ujjwal1103 <84891822+ujjwal1103@users.noreply.github.com>
2021-12-29 11:04:00 +0530

    Add files via upload

---
+++ dec20/script.js
@@ -0,0 +1,10 @@
+console.log("dec_20 Assignment No 1")
+//Write a program to check whether the number is greater than 10 or not. 
+var num;
+num = 5;
+	if(num>10){
+		console.log("Number Is Greater Than 10");
+	}
+	else{
+		console.log("Number is not Greater Than 10");
+	}
