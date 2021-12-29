cb63ac55791e350147618d9cf3f97c05f3a6eedc
ujjwal1103 <84891822+ujjwal1103@users.noreply.github.com>
2021-12-29 16:42:42 +0530

    Add files via upload

---
+++ dec23/Assignment2/script.js
@@ -0,0 +1,10 @@
+	console.log("dec_23 Assignment No 2")
+    //Write a program to print all even numbers less than or equal to n (n is an input which we take using prompt).
+    var n = parseInt(prompt("Enter where upto even number should be printed"));
+	
+	for(var i = 0; i<= n;i= i+1){
+		if(i%2 == 0){
+			console.log(i);
+		}
+	}
+	
