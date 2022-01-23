4a82d7426bd2a1cb725e639abf84c91449f4321f
ujjwal1103 <84891822+ujjwal1103@users.noreply.github.com>
2022-01-23 16:24:54 +0530

    Add files via upload

--- jan23test/index.js
+++ jan23test/index.js
@@ -1,1 +1,20 @@
+var dark = document.getElementById("dark");
+dark.addEventListener('click', darkEnable);
+dark.addEventListener('dblclick', lightEnable);
 
+function darkEnable(){
+    var nav = document.getElementById("nav")
+    var todos = document.getElementById("todos")
+    nav.style.backgroundColor = "white";
+    nav.style.color = "black";
+    todos.style.backgroundColor = "#333030";
+    
+}
+function lightEnable(){
+    var nav = document.getElementById("nav")
+    var todos = document.getElementById("todos")
+    nav.style.backgroundColor = "#333030";
+    nav.style.color = "white";
+    todos.style.backgroundColor = "#faebd7";
+    
+}
