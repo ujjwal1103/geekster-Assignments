f449412a3c3f013226d91246ca9b7f388e4bb851
ujjwal1103 <84891822+ujjwal1103@users.noreply.github.com>
2022-02-05 09:43:12 +0530

    Add files via upload

--- jan31/Assignment1/index.js
+++ jan31/Assignment1/index.js
@@ -1,7 +1,7 @@
 let arr = [2,7,11,15,14,5,9,6,3]
 let tar = parseInt(prompt("Enter target value"))
 let output = document.getElementById("output");
-if(tar < 20){
+
 for (let i = 0 ;i<arr.length;i++){
     if(arr[i]+arr[i+1]=== tar){
         output.innerHTML =` ${arr[i]} , ${arr[i+1]}`;
@@ -12,4 +12,4 @@ for (let i = 0 ;i<arr.length;i++){
         output.style.color = "red"
         break;
     }
-}}
+}
